25066e68307e2931ee82230305393f00240adc5d
jexorDev <3589164+jexorDev@users.noreply.github.com>
2024-05-07 11:14:39 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(() => {
+  axios.get('https://fidsapi.azurewebsites.net/AirportFlights').then((data) => {
+    console.log(data);
+  });
+});
+</script>
+
+<template>
+  <div style="background-color: 999999">blah</div>
+
+  <textarea class="terminal"></textarea>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.termainal {
+  bottom: 0;
+}
+</style>
