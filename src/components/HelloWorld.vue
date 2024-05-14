4a933cbae121e9617dd790be4a73b666059ccf8b
Dustin Seger <3589164+jexorDev@users.noreply.github.com>
2024-05-14 16:02:59 -0500

    show and load previous searches

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import axios from 'axios';
 import Flight from "../models/Flight";
 import GetFlightsResponseBody from '../models/GetFlightsResposneBody';
@@ -34,10 +34,24 @@ function loadNextPageFlights() {
     }).finally(() => status.value = "LOAD COMPLETE. ALL FLIGHTS LISTED ARE FOR TODAY.");
 }
 
+function executePreviousSearch(query: string) {
+  terminalQuery.value = query;
+  loadFlights();
+}
+
 function formatTime(date: Date): string {
   if (!date) return "";
   return new Date(date).toLocaleTimeString('en-us', { hour: "2-digit", minute: "2-digit"})
 }
+
+function buildFlightAwareLink(airline: string, flightNumber: string): string {
+  return `https://flightaware.com/live/flight/${airline}${flightNumber}`;
+}
+
+const previousSearchQueries = computed<string[]>(() => {
+  
+  return [...previousSearchResults.value.keys()];
+})
 
 const terminalPlaceholder = "-arriving frontier -between 16:00 17:30 -from den";
 const status = ref("")
@@ -71,7 +85,7 @@ const nextDataPageUrl = ref<string | null>(null);
       </tr>
       <tr v-for="flight in flights">
         <td>{{ flight.airlineName }}</td>
-        <td>{{ flight.airlineIdentifier }}{{ flight.flightNumber }}</td>
+        <td><a :href="buildFlightAwareLink(flight.airlineIdentifier, flight.flightNumber)" target="_blank">{{ flight.airlineIdentifier }}{{ flight.flightNumber }}</a></td>
         <td>{{ flight.cityName }}-{{ flight.cityAirportName }}</td>
 
         <td>{{ formatTime(flight.disposition == 0 ? flight.scheduledArrivalTime : flight.scheduledDepartureTime) }}</td>
@@ -86,7 +100,12 @@ const nextDataPageUrl = ref<string | null>(null);
       <a v-show="nextDataPageUrl" @click="loadNextPageFlights">LOAD MORE RESULTS</a>
   
     <input type="text" class="terminal" v-model="terminalQuery" v-on:keyup.enter="loadFlights" :placeholder="terminalPlaceholder"></input>
-
+    <div v-show="previousSearchQueries.length > 0">
+      PREVIOUS SEARCHES
+    </div>
+    <div v-for="query in previousSearchQueries">
+      <div @click="executePreviousSearch(query)" class="previous-search" v-text="query"></div>
+    </div>
   </div>
 </template>
 
@@ -114,6 +133,12 @@ table, th, td {
 .terminal {
   bottom: 0;
   width: 100%;
+}
+.previous-search {
+  color: white;
+  background-color: gray;
+  margin-top: 5px;
+  cursor: pointer;
 }
 th {
   text-align: left
